3eceb4297e493f667853e89a9ec93395f1a48668
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2022-04-04 20:36:18 +0200

    added dynamic projects to slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,43 +1,29 @@
 import Container from "./Container";
-import slide from '../img/slide.png'
 import "./Slider.css";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Controller, Navigation} from "swiper";
 import 'swiper/css';
+import {useEffect, useState} from "react";
+import Project from "./Project";
 
 export default function Slider() {
+    const [projects, setProjects] = useState([])
 
-    const slides1 = []
-    const slides2 = []
+    useEffect(() => {
+        fetch('http://localhost:4000/projects?_page=1&_limit=9')
+            .then(response => response.json())
+            .then(projects => setProjects(projects))
+    }, [])
 
-    for (let i = 0; i < 5; i++) {
-        slides1.push(
-            <SwiperSlide>
-                <div className="relative flex justify-center">
-                    <div className="group cursor-pointer">
-                        <img src={slide} alt=""/>
-                        <div className="absolute w-24 h-24 m-auto left-0 right-0 top-0 bottom-0 p-6 fill-white bg-green-500 rounded-full flex items-center justify-center invisible group-hover:visible">
-                            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="33"
-                                 height="42" viewBox="0 0 33 42">
-                                <path d="M32.546,10.358 L26.527,20.830 C25.577,22.483 23.473,23.049 21.828,22.095 L20.338,21.231 L22.058,18.239 L22.058,18.239 C22.881,18.716 23.933,18.433 24.408,17.607 L28.707,10.126 C29.182,9.300 28.900,8.244 28.078,7.767 L22.120,4.312 C21.297,3.835 20.246,4.118 19.771,4.944 L15.471,12.424 C14.996,13.250 15.278,14.307 16.100,14.784 L14.381,17.776 L12.891,16.912 C11.246,15.958 10.682,13.845 11.632,12.193 L17.652,1.720 C18.602,0.068 20.705,-0.498 22.351,0.456 L31.287,5.638 C32.932,6.592 33.496,8.705 32.546,10.358 ZM12.200,28.480 C11.377,28.003 11.096,26.946 11.571,26.120 L18.450,14.152 C18.925,13.325 19.977,13.042 20.799,13.519 C21.622,13.996 21.904,15.053 21.429,15.879 L14.549,27.848 C14.074,28.674 13.023,28.957 12.200,28.480 ZM10.941,23.760 L10.941,23.760 C10.118,23.283 9.067,23.567 8.592,24.393 L4.292,31.873 C3.817,32.699 4.099,33.756 4.921,34.233 L10.879,37.688 C11.702,38.165 12.754,37.882 13.229,37.056 L17.528,29.575 C18.003,28.749 17.721,27.692 16.899,27.215 L16.899,27.215 L18.619,24.223 L20.108,25.087 C21.753,26.041 22.317,28.154 21.367,29.807 L15.348,40.279 C14.398,41.932 12.294,42.498 10.649,41.544 L1.712,36.361 C0.067,35.407 -0.497,33.294 0.453,31.642 L6.473,21.169 C7.422,19.517 9.526,18.950 11.171,19.905 L12.661,20.768 L10.941,23.760 Z"/>
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-            </SwiperSlide>
-        )
-        slides2.push(
-            <SwiperSlide>
-                <div className="text-center">
-                    <h2 className="mb-6 text-lg">Dark UI Kit Freebie</h2>
-                    <p className="text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu erat lacus, vel
-                        congue mauris. Fusce velit justo, faucibus eu sagittis.
-                    </p>
-                </div>
-            </SwiperSlide>
-        )
-    }
+    if (projects.length === 0) return (
+        <div className="py-44">
+            <svg className="mx-auto animate-spin h-12 w-12 text-green-500" xmlns="http://www.w3.org/2000/svg"
+                 fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+            </svg>
+        </div>
+    )
 
     return (
         <div className="py-12">
@@ -53,7 +39,7 @@ export default function Slider() {
                 <div className="my-12">
                     <Swiper
                         loop={true}
-                        autoHeight={true}
+                        autoHeight={false}
                         modules={[Navigation, Controller]}
                         navigation={{
                             nextEl: '.swiper-button-next',
@@ -66,13 +52,19 @@ export default function Slider() {
                                 spaceBetween: 10
                             },
                             768: {
-                                slidesPerView: 4,
-                                spaceBetween: 40
+                                slidesPerView: 4.8,
+                                spaceBetween: 20,
                             },
                         }}
                         allowTouchMove={false}
                     >
-                        {slides1}
+                        {projects.map(proj => (
+                            <SwiperSlide key={proj.id}>
+                                <div className="max-w-[300px] mx-auto">
+                                    <Project img={proj.image} />
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
                 </div>
                 <Container>
@@ -94,7 +86,18 @@ export default function Slider() {
                                 slidesPerView={1}
                                 allowTouchMove={false}
                             >
-                                {slides2}
+                                {projects.map(proj => (
+                                    <SwiperSlide key={proj.id}>
+                                        <div className="text-center">
+                                            <h2 className="mb-6 text-lg">
+                                                {proj.name}
+                                            </h2>
+                                            <p className="text-sm">
+                                                {proj.description}
+                                            </p>
+                                        </div>
+                                    </SwiperSlide>
+                                ))}
                             </Swiper>
                         </div>
                         <button className="swiper-button-next col-start-12 col-span-1 md:col-start-10 ml-auto fill-green-500">
