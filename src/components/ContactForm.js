1cbe14bbd737055ca726e945dc9e910f48d6b7b4
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2022-04-06 00:13:23 +0200

    added recaptcha

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,21 +1,41 @@
 import {useForm} from "react-hook-form";
+import {GoogleReCaptcha, GoogleReCaptchaProvider} from 'react-google-recaptcha-v3';
 
 export default function ContactForm() {
-    const { register, handleSubmit, formState: { errors, isSubmitting, isSubmitSuccessful }, reset } = useForm();
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors,
+            isSubmitting,
+            isSubmitSuccessful
+        },
+        reset,
+        setValue
+    } = useForm();
 
     async function onSubmit(data) {
         await new Promise(resolve => setTimeout(resolve, 2000));
     }
 
+    function handleVerify(token) {
+        setValue('recaptcha_token', token)
+    }
+
+
     return (
-        <>
+        <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_RECAPTCHA_KEY}>
+            <GoogleReCaptcha onVerify={handleVerify} />
+
             <h2 className="font-title text-2xl uppercase mb-6">Contact form</h2>
             <p className="mb-6 text-sm">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad delectus ea id impedit recusandae
                 repellendus suscipit tempore voluptate. Nihil, suscipit.
             </p>
+
             {!isSubmitSuccessful ?
                 <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)} noValidate>
+                    <input type="hidden" {...register("recaptcha_token")} />
                     <div className="max-w-[350px]">
                         <input
                             {...register("name", {
@@ -88,10 +108,13 @@ export default function ContactForm() {
                         aria-label="Send Message"
                     >
                         {isSubmitting &&
-                            <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                            <svg className="animate-spin -ml-1 mr-2 h-5 w-5 text-white"
+                                 xmlns="http://www.w3.org/2000/svg"
                                  fill="none" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"/>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                        strokeWidth="4"/>
+                                <path className="opacity-75" fill="currentColor"
+                                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
                             </svg>
                         }
                         Send Message
@@ -99,8 +122,10 @@ export default function ContactForm() {
                 </form>
                 :
                 <div className="flex flex-col items-center text-green-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor" strokeWidth={1}>
+                        <path strokeLinecap="round" strokeLinejoin="round"
+                              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
                     </svg>
                     <span className="font-bold mb-6">Form submitted successfully.</span>
                     <button
@@ -112,6 +137,6 @@ export default function ContactForm() {
                     </button>
                 </div>
             }
-        </>
+        </GoogleReCaptchaProvider>
     )
 }
